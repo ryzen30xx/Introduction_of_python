0a6f851d9f0ba2a6725b05d2b21acb3b3bef14fd
RYZEN <110721139+ryzen30xx@users.noreply.github.com>
2023-06-05 16:04:33 +0700

    Upload first code on class

---
+++ Homework 1/Python.py
@@ -0,0 +1,20 @@
+# import the library
+import turtle as t
+#input data
+degrees = 90
+unit = 90
+#start draw
+t.left(degrees)
+t.forward(unit)
+t.right(degrees)
+t.forward(unit)
+t.right(degrees)
+t.forward(unit)
+t.left(degrees)
+t.forward(unit)
+t.left(degrees)
+t.backward(unit)
+t.right(degrees)
+t.backward(unit)
+#exit If click to program
+t.exitonclick()
