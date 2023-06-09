20e50b7d9f0416bef9e5aa8670e05a1fcfa3b412
RYZEN <48115317+isharoverwhite@users.noreply.github.com>
2023-06-09 22:07:10 +0700

    add example code

---
+++ Python Example Code/Create Table with Python example.py
@@ -0,0 +1,12 @@
+# import module
+from tabulate import tabulate
+ 
+# assign data
+mydata = [
+    ['a', 'b', 'c'],
+      [12, 34, 56],
+      ['Geeks', 'for', 'geeks!']
+]
+ 
+# display table
+print(tabulate(mydata, tablefmt="grid"))
---
+++ Python Example Code/L01 Introduction/08Turtle_square.py
@@ -0,0 +1,16 @@
+# draw a square
+
+import turtle as t
+
+length = 40
+
+t.forward(length)
+t.left(90)
+t.forward(length)
+t.left(90)
+t.forward(length)
+t.left(90)
+t.forward(length)
+t.left(90)
+
+t.exitonclick()
