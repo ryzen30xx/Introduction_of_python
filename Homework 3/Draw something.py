4a54fb86917ae2313a11ef5edd588b4b54caea81
RYZEN <48115317+isharoverwhite@users.noreply.github.com>
2023-06-13 22:56:08 +0700

    modding

--- Homework 3/Draw something.py
+++ Homework 3/Draw something.py
@@ -1,7 +1,8 @@
 import turtle as t
 useroptions = str(input("What geometries do you want to draw: "))
-if useroptions not in ["triangle", "square"]: print("wrong request!"); exit()
-elif useroptions == "triangle": 
-    for i in range(3): t.forward(50); t.left(120)
-elif useroptions == "square":
-    for i in range(4): t.forward(50); t.left(90)
+if useroptions in ["triangle", "square"]:
+    if useroptions == "triangle": 
+        for i in range(3): t.forward(50); t.left(120)
+    elif useroptions == "square":
+        for i in range(4): t.forward(50); t.left(90)
+else: print("wrong request!")
