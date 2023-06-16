23c656c6dc5f9cd0e45c6130e5fba30dd908b487
RYZEN <110721139+ryzen30xx@users.noreply.github.com>
2023-06-16 10:44:51 +0700

    update

--- Week 4 code on class/game.py
+++ Week 4 code on class/game.py
@@ -7,7 +7,10 @@ while playing:
     else: 
         computer = random.choice(dict)
         if computer == user_choose: print("tie!")
-        elif computer == 'rock' and user_choose == "sissors": print("you lose")
+        elif computer == 'rock' and user_choose == "paper": print("you win")
         elif computer == 'sissors' and user_choose == "rock": print("you win")
         elif computer == 'paper' and user_choose == "sissors": print("you win")
         else: print("you lose")
+        option = input('do you want to play: ')
+        if option == 'n': playing = False
+        else: playing = True
