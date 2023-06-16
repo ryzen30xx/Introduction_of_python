b944fe8efc72ab52a5d735ceac658baedadb00aa
RYZEN <110721139+ryzen30xx@users.noreply.github.com>
2023-06-16 10:32:07 +0700

    upload code on class

---
+++ Week 4 code on class/game.py
@@ -0,0 +1,13 @@
+import random
+playing = True
+dict = ['rock', 'sissors', 'paper']
+while playing:
+    user_choose = input("choose rock, sissors, paper: ")
+    if user_choose not in dict: print("invalid input")
+    else: 
+        computer = random.choice(dict)
+        if computer == user_choose: print("tie!")
+        elif computer == 'rock' and user_choose == "sissors": print("you lose")
+        elif computer == 'sissors' and user_choose == "rock": print("you win")
+        elif computer == 'paper' and user_choose == "sissors": print("you win")
+        else: print("you lose")
