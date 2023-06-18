4931b5317206016cc3dd89ca93a8668655583ac5
RYZEN <110721139+ryzen30xx@users.noreply.github.com>
2023-06-18 15:46:50 +0700

    Upload latest code

---
+++ Homework 4/Excercise 8.py
@@ -0,0 +1,7 @@
+from random import randint
+min_number = int(input("enter min number: ")); max_number = int(input("enter max number: ")); condition = int(input("Do you want many random numbers: "))
+for i in range(condition):
+    randomnumber =  randint(min_number, max_number); 
+    if randomnumber == 0: print("Bad luck, no random values for you"); break
+    elif randomnumber == 7: continue
+    print(randomnumber)
