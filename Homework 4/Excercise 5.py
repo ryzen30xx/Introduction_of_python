b8a9ac8f62817bfc2167e010507e4b7b42c5131d
RYZEN <110721139+ryzen30xx@users.noreply.github.com>
2023-06-18 15:55:59 +0700

    Fix some errors

--- Homework 4/Excercise 5.py
+++ Homework 4/Excercise 5.py
@@ -1,6 +1,6 @@
 row = int(input("Enter number of rows:")); collumns = int(input("Enter number of collumns:"))
 #collumns calculation
-for i in range(collumns +1): 
-    for j in range(row + 1): print("*", end="") 
-    for k in range(row + 1): print("=", end="")
+for i in range(collumns): 
+    for j in range(row): print("*", end="") 
+    for k in range(row): print("=", end="")
     print()
