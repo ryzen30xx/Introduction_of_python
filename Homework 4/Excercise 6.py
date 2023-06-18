f18daba4d434d5867c79be7016e5dce84aec89c8
RYZEN <110721139+ryzen30xx@users.noreply.github.com>
2023-06-18 15:53:36 +0700

    Fix some bugs in ex 6

--- Homework 4/Excercise 6.py
+++ Homework 4/Excercise 6.py
@@ -2,9 +2,8 @@ from random import randint
 random_request = int(input("How many times do you want to random:")); lowest = int(input("Enter min number: ")); highest = int(input("Enter max number: ")); dict = []
 for i in range(random_request):
     random_number = randint(lowest, highest)
-    if random_number == 0: print("Bad luck, no random values for you"); break
+    if random_number == 0: print("Bad luck, no random values for you"); exit()
     elif random_number == 7: continue
     #record to store(dictionary) append have a function is record values to the dictionary
     dict.append(random_number)
-output = "Random: "+", ".join(str(random_number)for random_number in dict)
-print(output)
+print("Random: "+", ".join(str(random_number)for random_number in dict))
