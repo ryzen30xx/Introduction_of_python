cf6e0a572afbe9ee489028b6751110906a7993b8
RYZEN <48115317+isharoverwhite@users.noreply.github.com>
2023-06-22 18:27:20 +0700

    Update new ways of Excercise 5

---
+++ Homework 4/Excercise 5 way 2.py
@@ -0,0 +1,6 @@
+row = int(input("Enter number of rows:")); collumns = int(input("Enter number of collumns:"))
+#collumns calculation
+for i in range(collumns): 
+    print("*" * row, end="") 
+    print("=" * row, end="")
+    print()
