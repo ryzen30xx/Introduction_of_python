025ac34350f8883c444f9f8baed26d536d287d08
RYZEN <110721139+ryzen30xx@users.noreply.github.com>
2023-07-11 02:38:12 +0700

    re-factory

--- Homework 6/Guest List.py
+++ Homework 6/Guest List.py
@@ -4,7 +4,4 @@ guest_num  = int(input("How many guests will be attending the party? "))
 for i in range(1, guest_num + 1): guest_list.append(input(f"Enter the name of guest {i}:"))
 print(f"The final guest list is: {guest_list}")
 remove_question = input("Do you want to remove any guests from the list? (yes/no): ")
-if remove_question.lower == "yes":
-    guest_remove = input("Enter the name of the guest to remove: ")
-    print("wtf guest ?") if guest_remove not in guest_list else guest_list.remove(guest_remove);print(f"Guest {guest_remove} has been removed from the list.");print(f"The updated guest list is: {guest_list}")
-else: print(f"Last guest list: {guest_list}")
+guest_remove = input("Enter the name of the guest to remove: "); print("wtf guest ?") if guest_remove not in guest_list else guest_list.remove(guest_remove);print(f"Guest {guest_remove} has been removed from the list.");print(f"The updated guest list is: {guest_list}") if remove_question.lower == "yes" else print(f"Last guest list: {guest_list}")
--- Homework 6/shop list.py
+++ Homework 6/shop list.py
@@ -3,9 +3,6 @@ print("Enter the items you want to buy (press Enter to finish):")
 for i in list: count += 1; print(f"{count}: {i}")
 choose_item = input("Which item? (all/index): ")
 if choose_item.replace(" ", "").isalpha() or choose_item.replace(" ", "").isdigit():
-    if choose_item.replace(" ", "").isalpha():
-        if choose_item.lower() != "all":print(wtf_conditional)
-        else: print(f"Your Shopping List: {list}")
-    else: 
-        print(wtf_conditional) if int(choose_item) > len(list) else print(f"Your item: {list[int(choose_item) - 1]}")
+    if choose_item.replace(" ", "").isalpha(): print(wtf_conditional) if choose_item.lower() != "all" else print(f"Your Shopping List: {list}")
+    else: print(wtf_conditional) if int(choose_item) > len(list) else print(f"Your item: {list[int(choose_item) - 1]}")
 else: print(wtf_conditional)
