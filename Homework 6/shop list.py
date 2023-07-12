304e7380b9dad0ce251fd4c4762426ba03fd896b
RYZEN <110721139+ryzen30xx@users.noreply.github.com>
2023-07-13 00:24:48 +0700

    modify some func

--- Homework 6/shop list.py
+++ Homework 6/shop list.py
@@ -1,10 +1,10 @@
-list = []; count = 0; wtf_conditional = "WTF input"; accept = True
+list = []; wtf_conditional = "WTF input"; accept = True
 print("Welcome to the Grocery Store! \nEnter the items you want to buy (press Enter to finish):")
 while accept: 
-    count += 1; insert_item = input(f"Item {count}:")
+    insert_item = input(f"Item {len(list) + 1}:")
     if insert_item != "": list.append(insert_item) 
-    else: accept = False; count = 0
-for i in list: count += 1; print(f"{count}: {i}")
+    else: accept = False
+for count, i in enumerate(list, start = 1): print(f"{count}: {i}")
 choose_item = input("Which item? (all/index): ")
 if choose_item.replace(" ", "").isalpha() or choose_item.replace(" ", "").isdigit():
     if choose_item.replace(" ", "").isalpha(): print(wtf_conditional) if choose_item.lower() != "all" else print(f"Your Shopping List: {list}")
