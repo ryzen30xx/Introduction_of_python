685d763113e07c456a706ec54a5cabe581424005
RYZEN <110721139+ryzen30xx@users.noreply.github.com>
2023-07-12 14:14:18 +0700

    refactory

--- Homework 6/shop list.py
+++ Homework 6/shop list.py
@@ -1,11 +1,9 @@
 list = []; count = 0; wtf_conditional = "WTF input"; accept = True
 print("Welcome to the Grocery Store! \nEnter the items you want to buy (press Enter to finish):")
 while accept: 
-    count += 1
-    insert_item = input(f"Item {count}:")
-    if insert_item != "": list.append(insert_item)
-    else: accept = False
-count = 0
+    count += 1; insert_item = input(f"Item {count}:")
+    if insert_item != "": list.append(insert_item) 
+    else: accept = False; count = 0
 for i in list: count += 1; print(f"{count}: {i}")
 choose_item = input("Which item? (all/index): ")
 if choose_item.replace(" ", "").isalpha() or choose_item.replace(" ", "").isdigit():
