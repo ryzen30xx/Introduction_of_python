5f5d6a118d40b7792a156879adeb0a13861214a9
RYZEN <110721139+ryzen30xx@users.noreply.github.com>
2023-07-12 10:25:38 +0700

    upload latest code

--- Homework 6/shop list.py
+++ Homework 6/shop list.py
@@ -1,17 +1,20 @@
 list = ["apple", "bread", "milk", "eggs"]; count = 0; wtf_conditional = "WTF_input"
-print("Enter the items you want to buy (press Enter to finish):")
+print("Enter the item you want to buy (press Enter to finish):")
 #to show an items in list
 for i in list: 
     count += 1 
     print(f"{count}: {i}")
 
-choose_item = input("Which item? (all/index): ")
+def alpha_case(choose_item):
+    if choose_item.lower() != "all":return(wtf_conditional)
+    else: return(f"Your Shopping List: {list}")
+
+def digit_case(choose_item):
+    if int(choose_item) > len(list):return(wtf_conditional)
+    else: return(f"Your item: {list[int(choose_item) - 1]}")
+
+choose_item = input("Which item? (all/index): ")    
 if choose_item.replace(" ", "").isalpha() or choose_item.replace(" ", "").isdigit(): #this line will check conditions if input condition is not alpha or not digits, application will be exit
-    if choose_item.replace(" ", "").isalpha():
-        if choose_item.lower() != "all":print(wtf_conditional)
-        else: print(f"Your Shopping List: {list}")
-    else: 
-        if int(choose_item) > len(list):
-            print(wtf_conditional) 
-        else: print(f"Your item: {list[int(choose_item) - 1]}")
-else: print(wtf_conditional)
+    print(alpha_case(choose_item)) if choose_item.replace(" ", "").isalpha() else print(digit_case((choose_item)))
+else: 
+    print(wtf_conditional)
