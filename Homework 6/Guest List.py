0a2281e4bdb9a11139b8d1b98903adef518bf4eb
RYZEN <110721139+ryzen30xx@users.noreply.github.com>
2023-07-12 11:01:29 +0700

    upload latest code

--- Homework 6/Guest List.py
+++ Homework 6/Guest List.py
@@ -1,13 +1,14 @@
 guest_list = []
 guest_num  = int(input("How many guests will be attending the party? "))
 
-for i in range(1, guest_num + 1): guest_list.append(input(f"Enter the name of guest {i}:"))
+for i in range(1, guest_num + 1): 
+    guest_list.append(input(f"Enter the name of guest {i}:"))
 
 print(f"The added guest list is: {guest_list}")
 
 remove_question = input("Do you want to remove any guests from the list? (yes/no): ")
 
-if remove_question.lower == "yes":
+if remove_question.lower() == "yes":
     guest_remove = input("Enter the name of the guest to remove: ")
     if guest_remove not in guest_list:
         print("wtf guest ?") 
