51f43b606d023d0e698430ab7f19d5c648ef77b9
RYZEN <110721139+ryzen30xx@users.noreply.github.com>
2023-07-12 13:54:23 +0700

    acceptable

--- Homework 6/shop list.py
+++ Homework 6/shop list.py
@@ -1,8 +1,25 @@
-list = ["apple", "break", "milk", "eggs"]; count = 0; wtf_conditional = "WTF input"
-print("Enter the items you want to buy (press Enter to finish):")
-for i in list: count += 1; print(f"{count}: {i}")
-choose_item = input("Which item? (all/index): ")
-if choose_item.replace(" ", "").isalpha() or choose_item.replace(" ", "").isdigit():
-    if choose_item.replace(" ", "").isalpha(): print(wtf_conditional) if choose_item.lower() != "all" else print(f"Your Shopping List: {list}")
-    else: print(wtf_conditional) if int(choose_item) > len(list) else print(f"Your item: {list[int(choose_item) - 1]}")
-else: print(wtf_conditional)
+list = []
+count  = 0
+val = True
+def add_item(item):
+    list.append(item)
+
+def process(condition):
+    if condition == "all":
+        return(f"Your Shopping List: {list}")
+    elif int(condition) > len(list):
+        return("wtf input")
+    elif condition.isdigit():
+        return(f"Your item: {list[int(condition) - 1]}")
+
+while val:
+    item = input("Enter the items you want to buy (press Enter to finish):")
+    if item == "":
+        val = False
+    else:
+        add_item(item)
+for i in list:
+    count += 1
+    print(f"Item {count}: {i}")
+choose_item = input("Which item? (all/[index]): ")
+print(process(choose_item))
